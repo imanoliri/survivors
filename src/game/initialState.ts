80ef9939973f70d641f340d3a1aa393d4feb3a7d
imanoliri <81101838+imanoliri@users.noreply.github.com>
2026-08-14 09:20:16 +0200

    Initialize tiles with buildings

--- src/game/initialState.ts
+++ src/game/initialState.ts
@@ -1,18 +1,18 @@
 import type { GameState, Tile } from './model';
 
 const tiles: Tile[] = [
-  { id: '0-0', x: 0, y: 0, terrain: 'forest' },
-  { id: '1-0', x: 1, y: 0, terrain: 'urban' },
-  { id: '2-0', x: 2, y: 0, terrain: 'water' },
-  { id: '3-0', x: 3, y: 0, terrain: 'farmland' },
-  { id: '0-1', x: 0, y: 1, terrain: 'farmland', ownerId: 'red' },
-  { id: '1-1', x: 1, y: 1, terrain: 'urban' },
-  { id: '2-1', x: 2, y: 1, terrain: 'forest' },
-  { id: '3-1', x: 3, y: 1, terrain: 'water' },
-  { id: '0-2', x: 0, y: 2, terrain: 'water' },
-  { id: '1-2', x: 1, y: 2, terrain: 'forest' },
-  { id: '2-2', x: 2, y: 2, terrain: 'urban' },
-  { id: '3-2', x: 3, y: 2, terrain: 'farmland', ownerId: 'blue' },
+  { id: '0-0', x: 0, y: 0, terrain: 'forest', buildings: [] },
+  { id: '1-0', x: 1, y: 0, terrain: 'urban', buildings: [] },
+  { id: '2-0', x: 2, y: 0, terrain: 'water', buildings: [] },
+  { id: '3-0', x: 3, y: 0, terrain: 'farmland', buildings: [] },
+  { id: '0-1', x: 0, y: 1, terrain: 'farmland', ownerId: 'red', buildings: [] },
+  { id: '1-1', x: 1, y: 1, terrain: 'urban', buildings: [] },
+  { id: '2-1', x: 2, y: 1, terrain: 'forest', buildings: [] },
+  { id: '3-1', x: 3, y: 1, terrain: 'water', buildings: [] },
+  { id: '0-2', x: 0, y: 2, terrain: 'water', buildings: [] },
+  { id: '1-2', x: 1, y: 2, terrain: 'forest', buildings: [] },
+  { id: '2-2', x: 2, y: 2, terrain: 'urban', buildings: [] },
+  { id: '3-2', x: 3, y: 2, terrain: 'farmland', ownerId: 'blue', buildings: [] },
 ];
 
 export const createInitialState = (): GameState => ({
